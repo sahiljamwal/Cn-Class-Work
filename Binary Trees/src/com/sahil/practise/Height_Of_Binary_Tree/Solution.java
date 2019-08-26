5b5710c5ca1f1b7e034024b5d3d792d86f5c3e74
Sahil Jamwal <34044226+sahiljamwal@users.noreply.github.com>
2019-08-26 21:06:04 +0530

    Update Solution.java

--- Binary Trees/src/com/sahil/practise/Height_Of_Binary_Tree/Solution.java
+++ Binary Trees/src/com/sahil/practise/Height_Of_Binary_Tree/Solution.java
@@ -46,36 +46,7 @@ public class Solution {
     
    
     
-	public static boolean checkBalanced(BinaryTreeNode<Integer> root){
-		
-		// Write your code here
-        boolean ans=false;
-        if(root==null)
-        {
-            ans=true;
-            return ans;
-        }
-        
-        int leftDepth=0,rightDepth=0;
-        leftDepth=height(root.left);
-        rightDepth=height(root.right);
-         
-        
-        if((Math.abs(leftDepth-rightDepth))<=1)
-        {
-            boolean a=checkBalanced(root.left);
-            boolean b=checkBalanced(root.right);
-            if(a && b)
-            {
-                ans=true;
-                return ans;
-            }
-            
-        }
-        
-    
-            return ans;
-    }
+	
 	
 }
 
